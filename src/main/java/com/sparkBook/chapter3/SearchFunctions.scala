85053c191ab04c00eb10557dd4613b3f2700dc3d
wensiyang <2251643104@qq.com>
2018-11-08 16:25:52 +0800

    spark rdd 行为操作

---
+++ src/main/java/com/sparkBook/chapter3/SearchFunctions.scala
@@ -0,0 +1,42 @@
+package com.sparkBook.chapter3
+
+import org.apache.spark.rdd.RDD
+
+
+class SearchFunctions(val query: String) {
+
+  def isMatch(s: String): Boolean = {
+
+    s.contains(query)
+
+  }
+
+  def getMatchesFunctionReference(rdd: RDD[String]): RDD[String] = {
+    rdd.filter(isMatch)
+  }
+
+  def getMatchesFieldReference(rdd: RDD[String]): RDD[String] = {
+    // 问题:"query"表示"this.query"，因此我们要传递整个"this"
+    rdd.flatMap(x => x.split(query))
+  }
+
+  def getMatchesNoReference(rdd: RDD[String]): RDD[String] = {
+    // 安全:只把我们需要的字段拿出来放入局部变量中
+    val query_ = this.query
+    rdd.flatMap(x => x.split(query))
+  }
+
+}
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    val search = new SearchFunctions("ERROR")
+    println(search.isMatch("我是1"))
+  }
+}
+
+
+
+
+
--- src/main/java/com/sparkBook/chapter3/TransformationDemo.scala
+++ src/main/java/com/sparkBook/chapter3/TransformationDemo.scala
@@ -3,16 +3,38 @@ package com.sparkBook.chapter3
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * 常用TF算子
+  * 常用TF算子(变化操作)
   */
 object TransformationDemo {
 
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("local").setMaster("Local[*]")
+    val conf = new SparkConf().setAppName("local").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val kv1 = sc.parallelize(List(("A",1),("B",2),("C",3),("A",4)))
-    val kv2 = sc.parallelize(List(("A",4),("B",6),("C",3),("A",6)))
+    //    val kv1 = sc.parallelize(List(("A",1),("B",2),("C",3),("A",4)))
+    //    val kv2 = sc.parallelize(List(("A",4),("B",6),("C",3),("A",6)))
+    /*
+    *
+    * 对一个数据为{1, 2, 3, 3}的RDD进行基本的RDD转化操作
+    *
+    * */
+    //val rdd = sc.parallelize(Array(1, 2, 3, 3))
+    //rdd.map(x => x + 1).take(4)
+    //rdd.flatMap(x => x.to(3))
+    //rdd.filter(_ != 1)
+    //rdd.distinct()
+    //rdd.sample(false,0.5)
+    //  .foreach(println)
+    val rdd1 = sc.parallelize(Array(1, 2, 3))
+    val rdd2 = sc.parallelize(Array(3, 4, 5))
+    //并集
+    rdd1.union(rdd2)
+    //交集 3
+    rdd1.intersection(rdd2)
+    //移除一个rdd的内容(移除训练集)
+    rdd1.subtract(rdd2)
+      .foreach(print)
+
   }
 
 }
